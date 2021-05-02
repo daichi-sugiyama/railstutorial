54f55429d9b979b2f8af299bfbcfda45927855df
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-02 15:51:46 +0900

    userモデルのバリデーションテストを実装

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
+require 'securerandom'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name:"テストユーザ",email:"test@test.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = SecureRandom.hex(51)
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = SecureRandom.hex(244) + "@example.com"
+    assert_not @user.valid?
+  end
 end
