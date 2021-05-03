382718cc59808545d307f03a46b159f35e1416c9
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-03 09:45:35 +0900

    認証失敗時、フラッシュメッセージを出すように実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザー情報画面にリダイレクト
+    else
+      # エラーメッセージを作成
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destory
+  end
 end
