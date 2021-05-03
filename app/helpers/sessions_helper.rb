4f861c709668a949eb996d3058b35778168e2213
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-03 10:24:14 +0900

    headerのレイアウトをログイン状況によって修正

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,15 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 現在ログイン中のユーザーを返す（いる場合）
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
+
+  # ユーザがログインしている時true,そうではない時false
+  def logged_in?
+    !current_user.nil?
   end
 end
