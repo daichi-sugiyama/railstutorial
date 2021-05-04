01daacb1bf9bb8d6d6592847ecb120b80695edfc
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-04 18:21:15 +0900

    userモデルのeditとupdateをログインしているときのみ呼び出すように修正

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   # 編集に失敗したときのテスト
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {
@@ -23,6 +24,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name  = "Foo Bar"
