eeb5fd8a2fc79261da0258632ebe994b10660dee
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-05 11:34:03 +0900

    アカウント有効化コントローラーを作成

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -10,16 +10,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-    end
-  end
-
-  test "index as admin including pagination and delete links" do
-    log_in_as(@admin)
-    get users_path
-    assert_template 'users/index'
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
