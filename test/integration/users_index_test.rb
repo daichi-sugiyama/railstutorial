b24414eea4093ce166ca39ba0e819733be152162
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-07 14:44:55 +0900

    有効化の場合のみユーザーを表示するようにテスト

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -13,11 +13,18 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated
+        # 有効なユーザ
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        # 無効なユーザは表示されない
+        assert_select 'a[href=?]', user_path(user), false
       end
     end
+    # 削除をテスト
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
@@ -27,5 +34,21 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "show as user" do
+    log_in_as(@admin)
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      # /user/#{id}にアクセス
+      get user_path(user)
+      if user.activated
+        # 有効なユーザ
+        assert_template 'users/show' # showが表示される
+      else
+        # 無効なユーザ
+        assert_redirected_to root_url # root_urlにリダイレクト
+      end
+    end
   end
 end
