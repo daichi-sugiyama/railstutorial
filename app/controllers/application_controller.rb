8919c7c1822deb1a871f0347fe26ab05d260a0a5
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-05-08 11:14:55 +0900

    micropostのフォームを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+
+  private
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        # ログインしていない場合
+        store_location # アクセスしたURLをセッションに保存
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
